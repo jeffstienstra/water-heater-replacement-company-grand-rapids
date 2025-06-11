6defd6505230909875afc39ec3ec3eaa5baaa65f
jeff-stienstra <jeff.stienstra@envoyb2b.com>
2025-06-11 16:39:31 -0400

    add images, product pages, automate pricing cards

---
+++ src/data/waterHeaterPricing.ts
@@ -0,0 +1,74 @@
+export const pricingOptions = [
+	{
+		title: "40-Gallon Standard Tank Water Heater (Gas or Electric)",
+		price: "$1,475 Installed",
+		image: "natDraftGroup",
+		features: [
+			"Standard replacement in same location",
+			"Removal & disposal of old tank",
+			"All required utility connections (water, gas, venting, electric)*",
+			"All work to code, with permit"
+		]
+	},
+	{
+		title: "50-Gallon Standard Tank Water Heater (Gas or Electric)",
+		price: "$1,675 Installed",
+		image: "natDraftGroup",
+		features: [
+			"Standard replacement in same location",
+			"Removal & disposal of old tank",
+			"All required utility connections (water, gas, venting, electric)*",
+			"All work to code, with permit"
+		]
+	},
+	{
+		title: "40-Gallon Gas Power Vent Tank Water Heater",
+		price: "$1,995 Installed",
+		image: "powerVentGroup",
+		features: [
+			"Standard replacement in same location",
+			"Removal & disposal of old tank",
+			"All required utility connections (water, gas, venting, electric)*",
+			"All work to code, with permit"
+		]
+	},
+	{
+		title: "50-Gallon Gas Power Vent Tank Water Heater",
+		price: "$2,095 Installed",
+		image: "powerVentGroup",
+		features: [
+			"Standard replacement in same location",
+			"Removal & disposal of old tank",
+			"All required utility connections (water, gas, venting, electric)*",
+			"All work to code, with permit"
+		]
+	},
+	{
+		title: "Tankless Water Heater",
+		price: "Starting at $2,500",
+		image: "tanklessGroup",
+		features: [
+			"Standard replacement in same location",
+			"Removal & disposal of old unit",
+			"All required utility connections (water, gas, venting, electric)*",
+			"All work to code, with permit",
+			"Price varies based on existing setup and requirements",
+			"Tankless systems may require additional venting, gas, or electrical upgrades",
+			"Home visit required if upgrading from a tank to tankless water heater (due to additional gas and electrical requirements)"
+		]
+	},
+	{
+		title: "Heat Pump Water Heater",
+		price: "Starting at $2,600",
+		image: "hybridGroup",
+		features: [
+			"Standard replacement in same location",
+			"Removal & disposal of old unit",
+			"All required utility connections (water, gas, venting, electric)*",
+			"All work to code, with permit",
+			"Price varies based on existing setup and requirements",
+			"Heat pump systems may require additional venting, gas, or electrical upgrades",
+			"Home visit required if upgrading from a tank to heat pump water heater (due to additional electrical requirements)"
+		]
+	}
+];
