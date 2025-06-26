4aa60b7d80e43155da84abb6768142edd04158a6
jeff-stienstra <jeff.stienstra@envoyb2b.com>
2025-06-26 15:11:02 -0400

    update how it works with Quick-Price-Check

--- src/components/icons/QuestionMark.jsx
+++ src/components/icons/QuestionMark.jsx
@@ -1,13 +1,13 @@
-export default function QuestionMark({ className = '', invert = false, ...props }) {
+export default function QuestionMark({ className = '', width = "24", height = "24", viewbox = "0 0 24 24", invert = false, ...props }) {
   return (
     <>
       {invert ? (
         <svg
           className={className}
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
+          width={width}
+          height={height}
+          viewBox={viewbox}
           fill="currentColor"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -17,9 +17,9 @@ export default function QuestionMark({ className = '', invert = false, ...props 
         <svg
           className={className}
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
+          width={width}
+          height={height}
+          viewBox={viewbox}
           fill="none"
           stroke="currentColor"
           strokeWidth="2"
